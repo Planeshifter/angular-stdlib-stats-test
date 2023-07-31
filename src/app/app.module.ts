26bd1a877568e216d66aa64e03e88aaf3682fcfb
Philipp Burckhardt <pburckhardt@outlook.com>
2023-07-31 12:38:13 -0400

    Load stats package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { dists } from '@stdlib/stats/base';
 import { AppComponent } from './app.component';
+
+console.log( dists.normal.cdf( 2.0, 0.0, 1.0 ) );
 
 @NgModule({
   declarations: [
