6bdf90bbdc18902f351b66d7b8b4c52db5891125
Philipp Burckhardt <pburckhardt@outlook.com>
2023-08-01 19:55:56 -0400

    Update code to reproduce bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { dists } from '@stdlib/stats/base';
+import { kstest } from '@stdlib/stats';
 import { AppComponent } from './app.component';
 
 console.log( dists.normal.cdf( 2.0, 0.0, 1.0 ) );
+console.log( kstest( [ 2.0, 1.0, 5.0, -5.0, 3.0, 0.5, 6.0 ], 'normal', 0.0, 1.0 ) );
 
 @NgModule({
   declarations: [
